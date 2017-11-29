9f8b125f36171fc07731329b09d9d7f14dc8c103
romano-w <romanow@berea.edu>
2017-11-29 10:30:23 -0500

    Added driver file

---
+++ romanow-csc236FP-driver.py
@@ -0,0 +1,9 @@
+######################################################################
+# Author: Will Romano
+# username: romanow
+
+# Assignment: Final Project
+# Purpose: Implements Conway's Game of Life as a terminal program
+
+######################################################################
+
