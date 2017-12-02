823635543200add8582eb5aba1d386bc523e4ce6
romano-w <romanow@berea.edu>
2017-12-01 19:22:31 -0500

    Added terminal display and test wires

--- Matrix.py
+++ Matrix.py
@@ -10,6 +10,12 @@
 
 class Matrix:
     def __init__(self, width, height):
+
+        # 0 empty (black),
+        # 1 electron head (blue),
+        # 2 electron tail (red),
+        # 3 conductor (yellow).
+
         self.matrix = []
         self.width = width
         self.height = height
@@ -25,12 +31,28 @@ class Matrix:
         Displays the current matrix to the terminal for debugging purposes
         :return: None
         """
-        print("\033[92m")
-        print(self.height, self.width)
-        print(self.matrix)
-        for col in range(self.height):
-            tmp_str = ""
-            for row in range(self.width):
-                tmp_str += str(self.matrix[row][col]) + " "
-            print(tmp_str)
-        print("\033[0m")
+        print("")
+        for row in self.matrix:
+            for col in row:
+                if col == 0:
+                    print(col, end="")
+                elif col == 1:
+                    print("\033[34m" + "\033[1m" + str(col) + "\033[0m", end="")
+                elif col == 2:
+                    print("\033[31m" + "\033[1m" + str(col) + "\033[0m", end="")
+                elif col == 3:
+                    print("\033[92m" + "\033[1m" + str(col) + "\033[0m", end="")
+            print("")
+
+    def _num_neighbors(self):
+        """
+        Finds the number of neighbors for a given cell.
+        :return: Returns an int
+        """
+        
+
+
+
+
+
+
--- romanow-csc236FP-driver.py
+++ romanow-csc236FP-driver.py
@@ -15,7 +15,18 @@ def main():
     Add docstring
     :return: None
     """
-    matrix = Matrix(25, 50)
+    matrix = Matrix(30, 15)
+
+    # Top half of timer
+    matrix.matrix[6][10], matrix.matrix[6][11], matrix.matrix[6][12], matrix.matrix[6][13], matrix.matrix[6][14], matrix.matrix[6][15] = 2, 3, 3, 3, 3, 3
+    # Bottom half of timer
+    matrix.matrix[8][10], matrix.matrix[8][11], matrix.matrix[8][12], matrix.matrix[8][13], matrix.matrix[8][14], matrix.matrix[8][15] = 3, 3, 3, 3, 3, 2
+    # Ends of timer
+    matrix.matrix[7][9], matrix.matrix[7][16] = 1, 1
+    # Wire leading out
+    matrix.matrix[7][17], matrix.matrix[7][18], matrix.matrix[7][19], matrix.matrix[7][20], matrix.matrix[7][21], matrix.matrix[7][22] = 3, 3, 3, 3, 3, 3
+    matrix.matrix[7][23], matrix.matrix[7][24], matrix.matrix[7][25], matrix.matrix[7][26], matrix.matrix[7][27], matrix.matrix[7][28], matrix.matrix[7][29] = 3, 3, 3, 3, 3, 3, 3
+
     matrix.term_display()
 
 
