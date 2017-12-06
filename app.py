abf700bf3a4077e6b2619d3c57b5e897dc409659
romano-w <romanow@berea.edu>
2017-12-06 01:49:18 -0500

    experimenting

--- app.py
+++ app.py
@@ -14,10 +14,16 @@ import datetime
 
 app = Flask(__name__)
 
+@app.route("/")
+def homepage():
+    the_time = datetime.time().strftime("%A, %d %b %Y %l:%M %p")
 
-@app.route('/')
-def render_static(page_name):
-    return render_template('%s.html' % page_name)
+    html = render_template('homepage.html', time=the_time)
+    return html
+
+# @app.route('/')
+# def render_static(page_name):
+#     return render_template('%s.html' % page_name)
 
 
 # @app.route("/")
